d0078efcdbbc493d6cebf5c3278bc76b0b09d524
gregory goren <grggoren@gmail.com>
2017-05-20 15:03:40 +0300

    debug

--- seo/lmda_copetition_experiment.py
+++ seo/lmda_copetition_experiment.py
@@ -12,7 +12,7 @@ from model_running import cross_validator as cv
 
 def simulation(chosen_models,data_set_location,query_to_fold_index,score_file,c_d_loc,new_scores_path,models_path,budget_creator):
 
-    c = cm.competition_maker(2, budget_creator,score_file, 10, data_set_location, 0.1, chosen_models, query_to_fold_index,c_d_loc,new_scores_path,models_path)
+    c = cm.competition_maker(1, budget_creator,score_file, 10, data_set_location, 0.1, chosen_models, query_to_fold_index,c_d_loc,new_scores_path,models_path)
     return c.competition("/lv_local/home/sgregory/LTOR_MART_min_max/new_scores/final")
 
 
