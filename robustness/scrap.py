7d132d88f4c5f99a534a8b27962e7c9d8bdb03f3
gregory goren <grggoren@gmail.com>
2017-08-06 19:01:29 +0300

    svm test run

--- robustness/scrap.py
+++ robustness/scrap.py
@@ -4,7 +4,7 @@ import preprocess as p
 from sklearn.datasets import load_svmlight_file
 
 if __name__=="__main__":
-    data_set_location = "C:/study/svm_test"
+    data_set_location = "../../../svm_test"
     prep = p.preprocess(data_set_location)
     a,b=prep.index_features_for_competitors(True)
     X,y = prep.create_data_set_svm_rank(a,b)
