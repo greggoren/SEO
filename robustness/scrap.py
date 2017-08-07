3a74daa50e9de32b38ca5b62a87b3494260fa936
gregory goren <grggoren@gmail.com>
2017-08-08 00:48:35 +0300

    svm test run

--- robustness/scrap.py
+++ robustness/scrap.py
@@ -1,20 +1,17 @@
-import svm_solver as ss
+import SVM_SGD as ss
 import numpy as np
 import preprocess as p
 from sklearn.decomposition import PCA
 
 if __name__=="__main__":
-    data_set_location = "../../../svm_test/a/test.txt"
+    data_set_location = "../../../svm_test/a/features"
     """prep = p.preprocess(data_set_location)
     a,b=prep.index_features_for_competitors(True)
     X,y = prep.create_data_set_svm_rank(a,b)"""
     prep = p.preprocess(data_set_location)
     a,b,c=prep.retrieve_data_from_file(data_set_location)
     X,y = prep.create_data_set(a,b,c)
-    svm = ss.SVM(C=0.1)
-    matrix =np.matrix(X,copy=False)
-    pca = PCA(n_components=2,copy=False,svd_solver='arpack')
-    reduced = pca.fit_transform(matrix)
-    print pca.explained_variance_ratio_
-    svm.fit(reduced,np.array(y))
+    svm = ss.svm_sgd(C=0.1)
+    svm.fit(X,np.array(y))
+    print svm.w
 
